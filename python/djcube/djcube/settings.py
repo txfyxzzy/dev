ad4fa670c819a9c371d591d5c06cc35c56cdca20
txfyxzzy <txfyxzzy@gmail.com>
2016-03-21 19:11:47 +0800

    DEV-1: djbooks

--- python/djcube/djcube/settings.py
+++ python/djcube/djcube/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'books',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,14 @@ WSGI_APPLICATION = 'djcube.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE' : 'django.db.backends.mysql',
+        'NAME' : 'test',
+        'USER' : 'benz',
+        'PASSWORD' : '',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
     }
 }
 
