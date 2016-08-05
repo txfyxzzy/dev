5b1458808f771888f8a62189691210410374912d
txfyxzzy <txfyxzzy@gmail.com>
2016-08-05 18:15:49 +0800

    DJANGO-1: update settings

--- python/djcube/djcube/settings.py
+++ python/djcube/djcube/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'books',
+    'gunicorn',
+    #'gunicorn --worker-class=gevent isaced.wsgi:application'
 )
 
 MIDDLEWARE_CLASSES = (
