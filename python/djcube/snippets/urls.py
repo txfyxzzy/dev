270d47113ad3fb71c579a67b9868e187f53eeceb
txfyxzzy <txfyxzzy@gmail.com>
2016-11-24 18:16:26 +0800

    DJANGO-1: request & response, class-based view

--- python/djcube/snippets/urls.py
+++ python/djcube/snippets/urls.py
@@ -1,7 +1,20 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
     url(r'^snippets/$', views.snippet_list),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+    url(r'^snippetsapifun/$', views.snippet_list_ex),
+    url(r'^snippetsapifun/(?P<pk>[0-9]+)/$', views.snippet_detail_ex),
+    url(r'^snippetsapicls/$', views.SnippetList.as_view()),
+    url(r'^snippetsapicls/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+
+    url(r'^snippetsapiclsex/$', views.SnippetListEx.as_view()),
+    url(r'^snippetsapiclsex/(?P<pk>[0-9]+)/$', views.SnippetDetailEx.as_view()),
+    url(r'^snippetsapiclsexex/$', views.SnippetListExEx.as_view()),
+    url(r'^snippetsapiclsexex/(?P<pk>[0-9]+)/$', views.SnippetDetailExEx.as_view()),
+
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
