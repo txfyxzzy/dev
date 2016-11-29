bfcd0e1f80d8920bcfdc5d179d430a1a575c2d5c
benzheq <benzheq@opera.com>
2016-11-29 23:28:08 +0800

    DJANGO-1: REST tutorial 7 schema

--- python/djcube/snippets/urls.py
+++ python/djcube/snippets/urls.py
@@ -1,11 +1,15 @@
+from django.conf.urls import include
 from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
+
 from rest_framework import routers
 from rest_framework import renderers
-from django.conf.urls import include
-from snippets import views
+from rest_framework.schemas import get_schema_view
+from rest_framework.urlpatterns import format_suffix_patterns
 
+from snippets import views
 from snippets.views import SnippetViewSet, UserViewSet, api_root
+
+schema_view = get_schema_view(title='Pastebin API')
 
 snippet_list = SnippetViewSet.as_view({
     'get': 'list',
@@ -68,6 +72,8 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^$', views.api_root),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    url('^schema/$', schema_view),
 ]
 
 #urlpatterns = format_suffix_patterns(urlpatterns)
